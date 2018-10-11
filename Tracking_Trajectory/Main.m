b69d58b5d31062b704dc9c5c64f68f97f5637b60
Aral-Sarrafi <aral_sarrafi@student.uml.edu>
2018-10-11 14:35:20 -0400

    Update The Car.m
    The model has been changed. To match the model that is being used in udacity.

--- Tracking_Trajectory/Main.m
+++ Tracking_Trajectory/Main.m
@@ -2,60 +2,23 @@ clear all
 close all
 clc
 %% Creat your object
-initial_states=[1000 -1000 -pi/2 0];
-initial_inputs=[100 0];
+initial_states=[0 0 0 0];
+initial_inputs=[pi/10 12];
 
 Lambo=Car(initial_states,initial_inputs);
-Lambo.Goal=[-1500;1500];
-Lambo.obstcle_C=[0;0];
-Lambo.obstcle_R=400;
 
-Lambo.Obsplot;
-hold on
-simulation_time=5;
-
-phi_d=pi;
-figure(1);
-figure(2);
+N = 200;
 
 
+for i = 1: N
+    figure(1);
 
-input=[1;1];
-X=[];
-Y=[];
-State=[];
-Time=[];
-time=0;
-while (norm(input)>1)
-time=time+Lambo.ts;
-Time=[Time;time];
-
-
-input=Lambo.Controler;
-Lambo.update_input(input);    
-Lambo.update_state;
-
-figure(1);
-Lambo.Carplot;
-hold off
-xlim([-1800 1800])
-ylim([-1800 1800])
-
-[x,y,phi,si]=Lambo.state_unpack;
-State=[State;x y phi si];
-
-
-grid on;
-
-
-
-
-hold on
-plot(State(:,1),State(:,2),'Linewidth',2)
-hold off
-
-% figure(2);
-% plot(Time,State(:,4),'b');
-% hold on
-% xlim([0 max(Time)])
+    
+    Lambo.show;
+    xlim([-200 200])
+    ylim([-200 200])
+    Lambo.update_state;
+    
+    
+    
 end
